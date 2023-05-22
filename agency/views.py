93a825feab3a586dbb8bf8bbb7b4e73cdf8e17a9
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-05-23 01:42:45 +0300

    change vievs NewspaperListView add template_name, context_object_name and refactor templates

--- agency/views.py
+++ agency/views.py
@@ -78,7 +78,9 @@ class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
 class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
     paginate_by = 5
-    queryset = Newspaper.objects.all().select_related("newspaper")
+    queryset = Newspaper.objects.all().select_related("topic")
+    template_name = "agency/newspaper_list.html"
+    context_object_name = "newspaper_list"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(NewspaperListView, self).get_context_data(**kwargs)
