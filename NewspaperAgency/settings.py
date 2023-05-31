9fbd1f5577c5ff0412568897cd237c6a07124c19
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-05-31 15:40:28 +0300

    fix SECRET_KEY

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-+t^odhf+h6tqf#yig6agutsm#3o6q=@+!k2^&0ocl8w9^2i#0d")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
