613f7649ade37237defb1bee82078aee3487e25c
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-06-01 09:18:59 +0300

    change wsgi file for deploing

--- NewspaperAgency/wsgi.py
+++ NewspaperAgency/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewspaperAgency.settings")
+path = "C:/PycharmProjects/NewspaperAgency"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
