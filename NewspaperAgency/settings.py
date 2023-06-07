89fedccf20a8f620489ce5de02f090c39216a308
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-06-07 12:18:16 +0300

    fix settings

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-+t^odhf+h6tqf#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "newspapers-agency-65hd.onrender.com"]
 
 
 # Application definition
